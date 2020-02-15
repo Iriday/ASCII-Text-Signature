8aea07175eacd0a732ae5ccd6e9e9cb4b6c64690
Igor <GI_Something@protonmail.com>
2020-02-15 16:34:47 +0200

    feat: Change/increase font

--- src/signature/Main.kt
+++ src/signature/Main.kt
@@ -1,18 +1,61 @@
 package signature
 
 fun main() {
-    val input = readLine()!!.trim()
-
-    val verticalLine = createVerticalLine(input.length + 4, "*")
-
-    println(verticalLine)
-    println("* $input *")
-    println(verticalLine)
+    Main().run()
 }
 
-private fun createVerticalLine(times: Int, element: String): String {
+class Main {
+    private val sbTop = StringBuilder()
+    private val sbMid = StringBuilder()
+    private val sbBtm = StringBuilder()
+
+    fun run() {
+        println("Enter name and surname: ")
+        val input = readLine()!!.trim()
+
+        for (value in input) {
+            if (value.isLetter()) {
+                appendLetter(value.toLowerCase())
+                sbTop.append(' ')
+                sbMid.append(' ')
+                sbBtm.append(' ')
+            } else if (value == ' ') {
+                sbTop.append("     ")
+                sbMid.append("     ")
+                sbBtm.append("     ")
+            } else {
+                sbTop.append(' ')
+                sbMid.append(' ')
+                sbBtm.append(' ')
+            }
+        }
+
+        val verticalLine = createVerticalLine(sbTop.length + 5, "*")
+
+        println(verticalLine)
+        println("*  $sbTop *")
+        println("*  $sbMid *")
+        println("*  $sbBtm *")
+        println(verticalLine)
+    }
+
+    private fun appendLetter(letter: Char) {
+        var counter = 0
+        for (c in 'a'..'z') {
+            if (c == letter) {
+                sbTop.append(arrayTop[counter])
+                sbMid.append(arrayMid[counter])
+                sbBtm.append(arrayBtm[counter])
+                return
+            }
+            counter++
+        }
+    }
+}
+
+private fun createVerticalLine(length: Int, element: String): String {
     val line = StringBuilder()
-    repeat(times) {
+    repeat(length) {
         line.append(element)
     }
     return line.toString()
